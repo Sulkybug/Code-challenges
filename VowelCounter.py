60c6d97d25e2990c94d07a3518d684c79c482fbe
Sulkybug <122849768+Sulkybug@users.noreply.github.com>
2024-03-10 01:45:11 -0500

    Create VowelCounter.py

---
+++ VowelCounter.py
@@ -0,0 +1,29 @@
+'''
+You are in an English class, your teacher tells the class that vowels are the glue that hold words and sentences together. 
+They want to make sure you understand the importance of vowels in a sentence.  
+You are given example sentences and are to give a total amount of vowels that are in each sentence.
+
+Task: 
+Write a program that takes in a string as input, counts and outputs the number of vowels (A, E, I, O, U).
+
+Input Format: 
+A string (letters can be both uppercase or lower case).
+
+Output Format: 
+A number which represents the total number of vowels in the string.
+
+Sample Input: 
+this is a sentence
+
+Sample Output: 
+6
+'''
+
+sentence = input().lower()
+count= 0
+
+for i in sentence:
+ if i == "a" or i == "e" or i == "i" or i== "o" or i == "u":
+  count += 1
+
+print(count)
