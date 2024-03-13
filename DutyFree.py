c9f64d3a16c4e5de7cae45844855c0afb42eec56
Sulkybug <122849768+Sulkybug@users.noreply.github.com>
2024-03-13 18:41:49 -0500

    Create DutyFree.py

---
+++ DutyFree.py
@@ -0,0 +1,32 @@
+'''
+You make a purchase of souvenirs priced in Euros at a duty free store in the Rome airport. You didn't want to spend any more than 20 US Dollars on any specific item. 
+Can you go through your list and make sure you stayed under your limit? The conversion rate on this day is 1.1 US Dollars to 1 Euro.
+
+Task: 
+Evaluate each item that you purchased to make sure that you didn't spend more than $20 on that particular item. 
+If you did, you will need to go back to the store to return it.  
+
+Input Format: 
+An string of numbers separated by spaces that represent the prices of each of your items in Euros.
+
+Output Format: 
+A string that says 'On to the terminal' if you stayed under your cap, or 'Back to the store' if you spent too much money.
+
+Sample Input: 
+18 15.50 2 14
+
+Sample Output: 
+On to the terminal
+'''
+
+bill= input().split(" ")
+result =""
+
+for i in bill:
+  if float(i)*1.1 > 20:
+   result="Back to the store"
+   break
+  else:
+   result="On to the terminal"
+
+print(result)
